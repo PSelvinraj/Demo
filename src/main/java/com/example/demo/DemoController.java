6ee7304e6f806a19f0c0aeed17e9b8590b856e6e
Percy <percy.roofa@gmail.com>
2025-02-01 14:40:48 -0800

    Initial commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController  // Marks this class as a REST API Controller
+@RequestMapping("/api")  // Base URL path for this controller
+public class DemoController {
+
+    @GetMapping("/hello")  // Maps HTTP GET requests to "/api/hello"
+    public String sayHello() {
+        return "Hello from Microservice!";
+    }
+}
